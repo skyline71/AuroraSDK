ddbb3dae68ed576a49aa4171ee09bdbcd5a1ad8e
Roman Mozzherin <romanmozzherin2015@gmail.com>
2023-11-16 17:39:53 +0300

    New commit after lesson 5

---
+++ Task 5/translations/ru.auroraos.Task5.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
+<context>
+    <name>DefaultCoverPage</name>
+    <message>
+        <location filename="../qml/cover/DefaultCoverPage.qml" line="46"/>
+        <source>Task5</source>
+        <translation>Task5</translation>
+    </message>
+</context>
+<context>
+    <name>Page2</name>
+    <message>
+        <location filename="../qml/pages/Page2.qml" line="46"/>
+        <source>Подтвердить</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Page2.qml" line="47"/>
+        <source>Отменить</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
